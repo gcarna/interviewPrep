dd217b4a6066ed399da213807fff2fe62ce0f4ee
gcarna <giuseppe.online92@gmail.com>
2020-12-05 12:50:39 +0100

    added part two

--- src/src/adventofcode/Day4.java
+++ src/src/adventofcode/Day4.java
@@ -11,25 +11,7 @@ import java.util.HashSet;
 
 public class Day4 {
 
-    private static ArrayList<String> fromFileToArray(String filename){
-        BufferedReader reader;
-        ArrayList<String> arrayList = new ArrayList<>();
-
-        try {
-            reader = new BufferedReader(new FileReader(filename));
-            String line = reader.readLine();
-            while (line != null) {
-                arrayList.add(line);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return arrayList;
-    }
-
-    private static int day4(ArrayList<String> arr){
+    private static int day4(ArrayList<String> arr, String part){
         ArrayList<HashMap<String, String>> arrayOfMaps = new ArrayList<>();
         int passportIndex = 0;
         HashMap<String, String> hashMap = new HashMap<>();
@@ -56,41 +38,8 @@ public class Day4 {
             if(!passport.containsKey("hcl")) isValidPassport = false;
             if(!passport.containsKey("ecl")) isValidPassport = false;
             if(!passport.containsKey("pid")) isValidPassport = false;
-            Integer byr;
-            Integer iyr;
-            Integer eyr;
-            String hgt;
-            String hcl;
-            String ecl;
-            String pid;
-            try{
-                byr = Integer.parseInt(passport.get("byr").toString());
-                iyr = Integer.parseInt(passport.get("iyr").toString());
-                eyr = Integer.parseInt(passport.get("eyr").toString());
-                hgt = passport.get("hgt").toString();
-                hcl = passport.get("hcl").toString();
-                ecl = passport.get("ecl").toString();
-                pid = passport.get("pid").toString();
-            } catch (Exception e){
-                continue;
-            }
 
-            if(byr < 1920 || byr > 2002) isValidPassport = false;
-            if(iyr < 2010 || iyr > 2020) isValidPassport = false;
-            if(eyr < 2020 || eyr > 2030) isValidPassport = false;
-            if(hgt.matches("[0-9]+(cm|in)")) {
-                String unit = hgt.substring(hgt.length()-2);
-                Integer num = Integer.parseInt(hgt.substring(0, hgt.length() - 2));
-                if("cm".equals(unit)) {
-                    if (num < 150 || num > 193) isValidPassport = false;
-                }
-                if("in".equals(unit)) {
-                    if (num < 59 || num > 76) isValidPassport = false;
-                }
-            } else {isValidPassport = false;}
-            if(!hcl.matches("^#[0-9a-f]{6}$")) isValidPassport = false;
-            if(!ecl.matches("(amb|blu|brn|gry|grn|hzl|oth)")) isValidPassport = false;
-            if(!pid.matches("[0-9]{9}")) isValidPassport = false;
+            if("two".equals(part)) isValidPassport = partTwoValidityCheck(passport);
 
 
             if(isValidPassport) validPassportCount++;
@@ -99,8 +48,69 @@ public class Day4 {
         return validPassportCount;
     }
 
+    private static boolean partTwoValidityCheck(HashMap<String, String> passport){
+        Integer byr;
+        Integer iyr;
+        Integer eyr;
+        String hgt;
+        String hcl;
+        String ecl;
+        String pid;
+        boolean isValidPassport = true;
+            try {
+                byr = Integer.parseInt(passport.get("byr").toString());
+                iyr = Integer.parseInt(passport.get("iyr").toString());
+                eyr = Integer.parseInt(passport.get("eyr").toString());
+                hgt = passport.get("hgt").toString();
+                hcl = passport.get("hcl").toString();
+                ecl = passport.get("ecl").toString();
+                pid = passport.get("pid").toString();
+            } catch (Exception e) {
+                return false;
+            }
+            if (byr < 1920 || byr > 2002) isValidPassport = false;
+            if (iyr < 2010 || iyr > 2020) isValidPassport = false;
+            if (eyr < 2020 || eyr > 2030) isValidPassport = false;
+            if (hgt.matches("[0-9]+(cm|in)")) {
+                String unit = hgt.substring(hgt.length() - 2);
+                Integer num = Integer.parseInt(hgt.substring(0, hgt.length() - 2));
+                if ("cm".equals(unit)) {
+                    if (num < 150 || num > 193) isValidPassport = false;
+                }
+                if ("in".equals(unit)) {
+                    if (num < 59 || num > 76) isValidPassport = false;
+                }
+            } else {
+                isValidPassport = false;
+            }
+            if (!hcl.matches("^#[0-9a-f]{6}$")) isValidPassport = false;
+            if (!ecl.matches("(amb|blu|brn|gry|grn|hzl|oth)")) isValidPassport = false;
+            if (!pid.matches("[0-9]{9}")) isValidPassport = false;
+
+        return isValidPassport;
+    }
+
+    private static ArrayList<String> fromFileToArray(String filename){
+        BufferedReader reader;
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+            while (line != null) {
+                arrayList.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+
     public static void main(String[] args) {
         ArrayList<String> arr = fromFileToArray("day4.txt");
-        System.out.println(day4(arr));
+        System.out.println(day4(arr, "one") );
+        System.out.println(day4(arr, "two") );
     }
 }
